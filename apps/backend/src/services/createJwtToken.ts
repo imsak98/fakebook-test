cff74f05dbb612557212b09b1d286dd7a54124ae
imsak98 <imsak1998@gmail.com>
2023-02-24 01:31:28 +0530

    feat(backend): setup jwt
    created two functions of creating jwt and verifying jwt token

---
+++ apps/backend/src/services/createJwtToken.ts
@@ -0,0 +1,19 @@
+import * as jose from 'jose'
+
+export const createJwtToken = async (payload) => {
+
+    const secret = new TextEncoder().encode(
+        'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
+    )
+    const alg = 'HS256'
+
+    const jwt = await new jose.SignJWT(payload)
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setIssuer('admin')
+    .setAudience('admin')
+    .setExpirationTime('15s')
+    .sign(secret)
+    // console.log(jwt);
+    return jwt
+}
