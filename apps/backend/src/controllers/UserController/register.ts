21b44c9d799595cb984f96ac1b5956a608dcddbf
imsak98 <imsak1998@gmail.com>
2023-02-24 02:55:54 +0530

    feat(backend): created register api

--- apps/backend/src/controllers/UserController/register.ts
+++ apps/backend/src/controllers/UserController/register.ts
@@ -1,16 +1,48 @@
-import { Request, Response } from "express";
-import { IRegsitrationRequestParams } from "../../helper/types";
-import { dbclient } from "../../services/dbclient";
+import { Request, Response } from 'express';
+import { IRegsitrationRequestParams, IUserObject } from '../../helper/types';
+import { dbclient } from '../../services/dbclient';
+import * as bcrypt from 'bcryptjs';
+import { v4 as uuid } from 'uuid';
 
-export const resgister = (req:Request, res:Response) => {
+export const resgister = async (req: Request, res: Response) => {
+  const client = dbclient();
+  const {
+    name,
+    contact_no,
+    display_name,
+    email,
+    password,
+  }: IRegsitrationRequestParams = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const user_id = uuid();
+  const password_hash = bcrypt.hashSync(password, salt);
+  const sql_query = `INSERT INTO users (id, name, display_name, email, password, contact_no) 
+                        VALUES (
+                            "${user_id}",
+                            "${name}",
+                            "${display_name}",
+                            "${email}",
+                            "${password_hash}",
+                            "${contact_no}" );`;
 
-    const client = dbclient()
-    const { name, contact_no, display_name, email }: IRegsitrationRequestParams = req.body
-    
-    console.log(name)
-    client.query('', (err, rows, fields) => {
-        // console.log('fileds',fields)
-    });
+  client.query(sql_query, (err, rows, fields) => {
+    console.log('err', err);
+    if (err) return res.status(400).send(err);
 
-    return res.send({test:"test"});
-}
+    if (rows.affectedRows > 0) {
+      console.log('rows', rows);
+      const user: IUserObject = {
+        id: user_id,
+        active: true,
+        contact_no: contact_no,
+        created_at: Date.now().toString(),
+        display_name: display_name,
+        email: email,
+      };
+      return res.status(200).send({
+        message: 'user has been registered! Please login again to continue',
+        user: user,
+      });
+    }
+  });
+};
--- apps/backend/src/helper/types.ts
+++ apps/backend/src/helper/types.ts
@@ -2,7 +2,17 @@ export interface IRegsitrationRequestParams {
   name: string;
   display_name: string;
   email: string;
+  password: string;
   contact_no: string;
   created_at?: string;
   active?: boolean;
+}
+
+export interface IUserObject {
+  id: string;
+  display_name: string;
+  email: string;
+  contact_no: string;
+  created_at: string;
+  active: boolean;
 }
