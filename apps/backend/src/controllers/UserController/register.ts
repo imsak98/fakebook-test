be145390781800da8ad9edc3d1aa10e0702b45ab
imsak98 <imsak1998@gmail.com>
2023-02-24 00:52:28 +0530

    wip: user registration

--- apps/backend/src/controllers/UserController/register.ts
+++ apps/backend/src/controllers/UserController/register.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
+import { IRegsitrationRequestParams } from "../../helper/types";
 import { dbclient } from "../../services/dbclient";
 
 export const resgister = (req:Request, res:Response) => {
 
-    const client = dbclient();
-    // console.log(client)
-    client.query('SELECT * FROM users', (err, rows, fields) => {
-        // console.log('fileds',fields);
+    const client = dbclient()
+    const { name, contact_no, display_name, email }: IRegsitrationRequestParams = req.body
+    
+    console.log(name)
+    client.query('', (err, rows, fields) => {
+        // console.log('fileds',fields)
     });
 
     return res.send({test:"test"});
---
+++ apps/backend/src/helper/types.ts
@@ -0,0 +1,8 @@
+export interface IRegsitrationRequestParams {
+  name: string;
+  display_name: string;
+  email: string;
+  contact_no: string;
+  created_at?: string;
+  active?: boolean;
+}
