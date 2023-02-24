f4f0d8d1ba71ee1421aa85930ab83ce1e6b3fe80
imsak98 <imsak1998@gmail.com>
2023-02-24 13:53:56 +0530

    feat(backend): created auth middleware

--- apps/backend/src/middlewares/Auth.ts
+++ apps/backend/src/middlewares/Auth.ts
@@ -1,7 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { verifyJwtToken } from "../services/verifyJwtToken";
 
-export const Auth = (req:Request, res:Response, next:NextFunction) => {
-    console.log(req.headers.authorization);
-    if(req.headers.authorization === 'Bearer test') next();
-    else    res.status(401).send({error: "error"});
+export const Auth = async (req:Request, res:Response, next:NextFunction) => {
+    const authHeader = req.headers.authorization
+    
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        console.log(token);
+        const {payload , protectedHeader, code:err} = await verifyJwtToken(token)
+        if(err) return res.status(401).send({error: "Invalid token"})
+        if(payload){
+            req.body.user = payload.user
+            next()
+        }
+
+    } else 
+        return res.status(401).send({error: "Authorization header not found"});
 }
